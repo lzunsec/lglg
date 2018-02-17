f4467efb211b831fc10b33bc462b1e14fc31b2f1
me <contas@lucaszanella.com>
2018-02-17 19:24:39 -0200

    added yaml support

--- fgitian.py
+++ fgitian.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import libvirt
+import yaml
 
+yaml_path = "linux.yaml"
 image_xml_path = "ubuntu_base.xml"
 emulator = "qemu"
 connection = libvirt.open(emulator + ":///system")
@@ -12,6 +14,12 @@ connection.listDefinedDomains()
 xml = ""
 with open(image_xml_path, "r") as file:
 	xml = file.read()
+
+yaml_data = ""
+with open(yaml_path, "r") as file:
+    yaml_data = yaml.load(file)
+
+print(yaml_data)
 
 connection.defineXML(xml)
 
